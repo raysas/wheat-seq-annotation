8a34b887540e2749332c6e82bf0a83d59a27703a
raysas <rayane.s.adam@gmail.com>
2024-11-15 10:52:47 +0100

    -- fix foldrs + artemis

---
+++ utils/tools_installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# -- creates a directory if it doesn't exist to put it in tehre and keep it clean
+if [ ! -d tools/ ]; 
+    then mkdir tools; 
+fi 
+
+
+# -------------------- Genome Browser --------------------
+# <> Artemis Genome Browser (https://www.sanger.ac.uk/science/tools/artemis)
+#  (can also install it through conda)
+
+wget https://github.com/sanger-pathogens/Artemis/releases/download/v18.2.0/artemis-unix-release-18.2.0.tar.gz #downloads the tar file
+if [ ! -d tools/ ]; then mkdir tools; fi #creates a directory if it doesn't exist to put it in tehre and keep it clean
+
+mv artemis-unix-release-18.2.0.tar.gz tools/
+tar zxf tools/artemis-unix-release-18.2.0.tar.gz
+
